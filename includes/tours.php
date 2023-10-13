6047cb982779cbfe12330c162f8ede4e156f44c0
l.westhead <luke.westhead@canterbury.ac.uk>
2023-10-13 15:43:46 +0100

    tour button accessibility

--- includes/tours.php
+++ includes/tours.php
@@ -10,7 +10,7 @@
 									<p>A fascinating walk around Royal London including the Changing of the Guard, taking in the splendour and glamour of our capital. See three royal palaces, Westminster Abbey and Big Ben on this fully guided tour.</p> 
 									
 									<p>10:30 Monday, Wednesday, Friday and Sunday.</p>
-									<p><a class="btn btn-default" href="/tour/royal-london-and-changing-of-the-guard-tour" aria-label="View details or the Royal London and Changing of the Guard tour">View details »</a></p>
+									<p><a class="btn btn-default" href="/tour/royal-london-and-changing-of-the-guard-tour" aria-label="View details of the Royal London and Changing of the Guard tour">View details »</a></p>
 								</div>
 						</div>	
 	
@@ -20,7 +20,7 @@
 								<h3>Fire, Pestilence and Plague</h3>
 								<p>For hundreds of years the City of London has been ‘plagued’ by disaster, from the sacking of the city by Queen Boudicca, to the bomb damage during the Blitz. Along the way you’ll encounter mass graveyards from the plague, execution sites and sinister stories. A tour not to be missed!</p>
 								<p>Wednesday and Sunday 2:30pm.</p>
-								<p><a class="btn btn-default" href="/tour/fire-pestilence-and-plague-tour">View details »</a></p>
+								<p><a class="btn btn-default" href="/tour/fire-pestilence-and-plague-tour" aria-label="View details of the Fire, Pestilence and Plague tour">View details »</a></p>
 							</div>
 						</div>	
 	
@@ -30,7 +30,7 @@
 								  <h3>Debauched London! Bankside, Brothels and Beer</h3>
 								  <p>Experience the real history of London, the people’s history! Southwark has long been associated with debauchery, but has recently enjoyed a resurgence in popularity. Join our expert guide on this unique tour of Bankside and discover its compelling history.</p>
 								  <p>Saturday 2:30pm.</p>
-								  <p><a class="btn btn-default" href="/tour/debauched-london-bankside-brothels-and-beer-tour">View details »</a></p>
+								  <p><a class="btn btn-default" href="/tour/debauched-london-bankside-brothels-and-beer-tour" aria-label="View details of the Debauched London! Bankside, Brothels and Beer Tour">View details »</a></p>
 								</div>
 						</div>	
 			</div>
@@ -43,8 +43,7 @@
 							<img src="images/soho-covent-garden-small.jpg">
 								<div class="hp-tour">
 									<h3>Soho and Covent Garden, the Wicked West End</h3>
-									<p>See where the Beatles and the Rolling Stones performed and where the infamous rooftop concert took place (the Beatles last together). Discover the glamour of Theatreland and the nightspots, pubs and bars that everyone visiting London should see. Every Wednesday 2:30 PM from the Statue of Eros (Piccadilly Circus).</p>
-									<p><a class="btn btn-default" href="/tour/soho-and-covent-garden-the-wicked-west-end-tour" role="button">View details »</a></p>
+									<p>See where the Beatles and the Rolling Stones performed and where theSoho and Covent Garden, the Wicked West End tour">View details »</a></p>
 								</div>
 						</div>-->	
 	
@@ -54,7 +53,7 @@
 								<h3>Secrets of London</h3>
 								<p>Experience a walking tour with a difference! On the secret hidden London tour you will hear the more unusual stories about London’s famous sights. Discover the secrets of our famous city that are hiding in plain view!</p>
 								<p>Thursday 2.30pm.</p>
-								<p><a class="btn btn-default" href="/tour/secrets-of-london-tour">View details »</a></p>
+								<p><a class="btn btn-default" href="/tour/secrets-of-london-tour" aria-label="View details of the Secrets of London tour">View details »</a></p>
 							</div>
 						</div>	
 	
@@ -64,7 +63,7 @@
 								  <h3>Jack the Ripper!</h3>
 								  <p>The autumn of 1888 will forever be known in London as the Autumn of Terror. In London’s East End a serial killer stalked the streets, murdering prostitutes and mutilating their bodies, his name, Jack the Ripper!</p>
 								  <p>Friday 7pm.</p>
-								  <p><a class="btn btn-default" href="/tour/jack-the-ripper-tour">View details »</a></p>
+								  <p><a class="btn btn-default" href="/tour/jack-the-ripper-tour" aria-label="View details of the Jack the Ripper! tour">View details »</a></p>
 								</div>
 						</div>	
 			</div>
