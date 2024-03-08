a10a8b5648c40925654c85c224fc57af432eab57
l.westhead <luke.westhead@canterbury.ac.uk>
2024-03-08 18:47:25 +0000

    Alert text update

--- includes/alert.php
+++ includes/alert.php
@@ -1,9 +1,9 @@
 <div class="container-fluid alert-bg">
 			<div class="row no-row">
 				<div class="col-md-12 alert">
-                   <h1>Due to unforeseen circumstances, All tours have been canceled</h1>
+                   <h1>Due to unforeseen circumstances, All tours have been canceled.</h1>
                     
-                    <h1>We apologise for any inconvenience</h1>
+                    <h1>We apologise for any inconvenience.</h1>
                 </div>
     </div> 
 </div>   
